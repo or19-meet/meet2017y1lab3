b718f9290e56db54a752313c9be7cc9225904ee0
or19-meet <or19@meet.mit.edu>
2017-07-25 17:58:48 +0300

    Stopping here

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+print("שלום עולם is how to say hello world in Hebrew")
+print("Γειά σου Κόσμε is how to say hello world in Greek")
+print("Olá Mundo is how to say HEllo World in Portuguese")
+print("Ahoj světe is how to say hello world in Czech")
+print("안녕 세상 is how to say hello world in Korean")
+print("你好，世界 is how to say hello world in Chinese")
+print("नमस्ते दुनिया is how ti say hello world in Hindi")
+print("Salamu, Dunia is how to say hello world in Swahili")
+
+
+
